ba9d7f7f66541723cd32881ef4cc8af48a45341a
John <alexpatersonsmail@gmail.com>
2020-01-01 13:12:34 +0000

    first

---
+++ ApiSwitchingIssue/Demo.cs
@@ -0,0 +1,146 @@
+﻿using Veldrid;
+using Veldrid.Sdl2;
+using Veldrid.Utilities;
+using Veldrid.StartupUtilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ApiSwitchingIssue
+{
+    class Demo
+    {
+        private Sdl2Window _window;
+        private GraphicsDevice _device;
+        private DisposeCollectorResourceFactory _factory;
+        private CommandList _cl;
+
+        private List<GraphicsBackend> _apis;
+        private int _apiIndex;
+
+        private int _frameCount = 0;
+        private const int _periodNumberFrames = 120;
+
+        public void Run()
+        {
+            ExtractValidAPIs();
+
+            Init();
+
+            Loop();
+
+            ReleaseResources();
+        }
+
+        private void ExtractValidAPIs()
+        {
+            //Comment those you want to include in the rotation (if valid for system)
+            var apisExcluded = new List<GraphicsBackend>
+            {
+                GraphicsBackend.OpenGLES,
+                //GraphicsBackend.OpenGL,
+                GraphicsBackend.Direct3D11,
+                //GraphicsBackend.Vulkan,
+                GraphicsBackend.Metal
+            };
+
+            _apis = ((GraphicsBackend[])Enum.GetValues(typeof(GraphicsBackend))).Where(x => !apisExcluded.Contains(x) && GraphicsDevice.IsBackendSupported(x)).ToList();
+
+            _apiIndex = 0;
+        }
+
+        private void Init()
+        {
+            WindowCreateInfo windowCI = new WindowCreateInfo()
+            {
+                X = 100,
+                Y = 100,
+                WindowWidth = 960,
+                WindowHeight = 540,
+                WindowTitle = "Potential Issue Switching APIs"
+            };
+
+            _window = VeldridStartup.CreateWindow(ref windowCI);
+
+            ReCreateGraphicsDevice();
+        }
+
+        private void ReCreateGraphicsDevice()
+        {
+            _factory?.DisposeCollector.DisposeAll();
+            _device?.Dispose();
+
+            var options = new GraphicsDeviceOptions(
+              debug: true,
+              swapchainDepthFormat: PixelFormat.R16_UNorm,
+              syncToVerticalBlank: true,
+              resourceBindingModel: ResourceBindingModel.Improved);
+
+            _device = VeldridStartup.CreateGraphicsDevice(_window, options, _apis[_apiIndex]);
+            _factory = new DisposeCollectorResourceFactory(_device.ResourceFactory);
+            _cl = _factory.CreateCommandList();
+
+            //ERROR on change from OpenGL to Vulkan     
+            //With debug : false -> it is a memory access violation
+            //With debug : true -> it is Veldrid.VeldridException. HResult = 0x80131500. Message = A Vulkan validation error was encountered: [ErrorEXT](SwapchainKHREXT) vkCreateSwapchainKHR: internal drawable creation failed
+        }
+
+        private void Loop()
+        {
+            while (_window.Exists)
+            {
+                _window.PumpEvents();
+                Update();
+                Render();
+            }
+
+            ReleaseResources();
+        }
+
+        private void Update()
+        {
+            _frameCount++;
+
+            if (_frameCount == _periodNumberFrames)
+            {
+                _frameCount = 0;
+
+                var currentAPI = _apis[_apiIndex].ToString();
+
+                _apiIndex++;
+                if (_apiIndex == _apis.Count)
+                {
+                    _apiIndex = 0;
+                }
+
+                var nextAPI = _apis[_apiIndex].ToString();
+
+                System.Diagnostics.Debug.WriteLine(string.Concat("Trying to switch from ", currentAPI, " to ", nextAPI));
+                ReCreateGraphicsDevice();
+                System.Diagnostics.Debug.WriteLine("Success!");
+            }
+        }
+
+        private void Render()
+        {
+            _cl.Begin();
+
+            _cl.SetFramebuffer(_device.SwapchainFramebuffer);
+
+            _cl.ClearColorTarget(0, RgbaFloat.CornflowerBlue);
+
+            _cl.End();
+
+            _device.SubmitCommands(_cl);
+
+            _device.SwapBuffers();
+
+            _device.WaitForIdle();
+        }
+
+        private void ReleaseResources()
+        {
+            _factory.DisposeCollector.DisposeAll();
+        }
+    }
+}
---
+++ ApiSwitchingIssue/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ApiSwitchingIssue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Demo to demonstrate potential problems switching Backend APIs, in particular going 'back' to Veldrid after OpenGL");
+
+            var demo = new Demo();
+
+            demo.Run();
+        }
+    }
+}
